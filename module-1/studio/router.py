cf0043e9ed13d09d5a098936200266adce51921c
sfujimotosfujimoto <unicoshun@gmail.com>
2025-10-26 22:03:01 +0900

    update ammend

---
+++ module-1/studio/router.py
@@ -0,0 +1,42 @@
+# from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+from langgraph.graph import END, START, MessagesState, StateGraph
+from langgraph.prebuilt import ToolNode, tools_condition
+
+
+# Tool
+def multiply(a: int, b: int) -> int:
+    """Multiplies a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+
+# LLM with bound tool
+llm = ChatOllama(model="gemma3:12b", temperature=0)
+llm_with_tools = llm.bind_tools([multiply])
+
+
+# Node
+def tool_calling_llm(state: MessagesState):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+
+# Build graph
+builder = StateGraph(MessagesState)
+builder.add_node("tool_calling_llm", tool_calling_llm)
+builder.add_node("tools", ToolNode([multiply]))
+builder.add_edge(START, "tool_calling_llm")
+builder.add_conditional_edges(
+    "tool_calling_llm",
+    # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
+    # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
+    tools_condition,
+)
+builder.add_edge("tools", END)
+
+# Compile graph
+graph = builder.compile()
